87fb71092171ae23e37935ea303174d656789e3f
Sereno <sereno.dominguez@blackcape.io>
2024-12-07 00:56:27 -0500

    add max's tests

--- tests/test_player.py
+++ tests/test_player.py
@@ -1,6 +1,7 @@
 import unittest
 from unittest.mock import Mock
 from src.server.player_class import Player
+import socket
 
 class TestPlayer(unittest.TestCase):
     def setUp(self):
@@ -24,3 +25,18 @@ class TestPlayer(unittest.TestCase):
         different_socket = Mock()
         player3 = Player(different_socket)
         self.assertNotEqual(self.player, player3)
+
+        def test_player_str(self):
+            expected_str = f"player {self.player.name} in game {self.player.curr_game} {self.mock_sock.getpeername()}"
+            self.assertEqual(str(self.player), expected_str)
+
+        def test_player_equality_with_other_player(self):
+            mock_sock2 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            player2 = Player(mock_sock2)
+            self.assertNotEqual(self.player, player2)
+
+        def test_player_equality_with_socket(self):
+            self.assertEqual(self.player, self.mock_sock)
+
+        def test_player_equality_with_id(self):
+            self.assertEqual(self.player, self.player.id)
